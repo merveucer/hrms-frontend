837ab0c4ada0bef6690d862f4bd998aed032a7a1
merveucer <merveucer.txt@gmail.com>
2021-08-11 18:01:17 +0300

    Install react router dom and wrap main component with browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <App />,
+  <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
 );
 
